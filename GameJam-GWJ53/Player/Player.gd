227c101ee09aa8499a9a564ada871ab3674b3a95
goodguyplayer <nathanbritodasilva@gmail.com>
2023-01-14 00:59:56 -0200

    Player code updated - INow falls and jumps

--- GameJam-GWJ53/Player/Player.gd
+++ GameJam-GWJ53/Player/Player.gd
@@ -1,16 +1,33 @@
 extends KinematicBody2D
 
+
 export (int) var speed = 200
+export (int) var gravity = 400
+export (int) var jump_speed = -250
 
 var velocity = Vector2()
 var movement = 0
 
 func get_input():
-	velocity = Vector2.ZERO
 	velocity.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
+	velocity.x = velocity.x * speed
 	
 
 
 func _physics_process(delta):
 	get_input()
-	velocity = move_and_slide(velocity * speed)
+	velocity.y += gravity * delta
+	velocity = move_and_slide(velocity, Vector2.UP)
+	if Input.is_action_just_pressed("ui_up"):
+		if is_on_floor():
+			velocity.y = jump_speed
+
+
+
+
+
+
+#####
+# Resources used.:
+
+# Platform Character = https://kidscancode.org/godot_recipes/3.x/2d/platform_character/
