5d4e9faae20a2d5fe2e6c7cb4804740d85b3df35
goodguyplayer <nathanbritodasilva@gmail.com>
2023-01-21 17:39:43 -0200

    Creating victory and defeat scenes

---
+++ GameJam-GWJ53/Menu/Scenes/Victory.gd
@@ -0,0 +1,20 @@
+extends Control
+
+
+onready var score = get_node("MarginContainer/VBoxContainer/Margin-Score/VBoxContainer/HBoxContainer2/Soldiers killed")
+onready var time_spent = get_node("MarginContainer/VBoxContainer/Margin-Score/VBoxContainer/HBoxContainer/TimeSpent")
+
+var minutes = 0
+var seconds = 0
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	time_calculator()
+	score.text = str(GlobalVariables.enemies_killed) + " out of 20"
+	time_spent.text = str(minutes) + ":" + str(seconds)
+	
+
+func time_calculator():
+	minutes = GlobalVariables.time_spent / 60
+	seconds = GlobalVariables.time_spent % 60
+	
